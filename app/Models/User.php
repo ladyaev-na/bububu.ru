77cc5f6f78a9a9d3a6660a9e6f30d88ae12950ba
ladyaev-na <LADYAYEV@INBOX.RU>
2024-11-06 17:25:11 +0700

    API register login logout

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,42 +6,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    use HasFactory, Notifiable, HasApiTokens;
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'surname', 'name', 'patronymic', 'sex', 'birthday',
+        'email', 'password', 'nickname','avatar', 'phone',
+        'api_token', 'role_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
-        'remember_token',
+        'api_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
